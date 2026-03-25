87e9ca34b9e693084c9970bf62a99b3869e2f52d
Gordeych1k <hkonashenkov@gmail.com>
2026-03-25 20:37:09 +0200

    .

---
+++ unittests/age.py
@@ -0,0 +1,5 @@
+def check_age(age):
+    if age >=18:
+        return True
+    else:
+        return False
---
+++ unittests/test_age.py
@@ -0,0 +1,3 @@
+from age import check_age
+assert check_age(24) == True
+assert check_age(17) == False
